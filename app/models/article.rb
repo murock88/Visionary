ae537a134b00bf5435c0a15a0ba6a5a03eba5217
murock88 <devildogmurock@gmail.com>
2017-04-02 04:03:20 +0000

    validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ActiveRecord::Base
   
   belongs_to :user
   has_many :comments
+  
+  
+   validates :title, length: { minimum: 5, message: "This article title is too short"}   # I did that <---- :)
+   validates :title, uniqueness: true, uniqueness: { message: "This article title has already been
+posted."}
+   validates :body, length: { minimum: 15 }
+ 
+ 
 end
