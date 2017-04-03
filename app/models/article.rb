27a307997f0e3e1b7d024a95249e6d680a9e11e7
murock88 <devildogmurock@gmail.com>
2017-04-03 01:14:48 +0000

    added css and shit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,11 @@
 class Article < ActiveRecord::Base
   
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy #this destroys comments when user is deleted
   
   
-   validates :title, length: { minimum: 5, message: "This article title is too short"}   # I did that <---- :)
-   validates :title, uniqueness: true, uniqueness: { message: "This article title has already been
-posted."}
+   validates :title, length: { minimum: 5, message: "This article title is too short" }   # I did that <---- :)
+   validates :title, uniqueness: true, uniqueness: { message: "This article title has already been posted." }
    validates :body, length: { minimum: 15 }
  
  
